4f79c6f0a0952ee6044b1fb6616197966c6d0cd1
zeeshan <zshaan470@gmail.com>
2019-07-25 17:56:46 +0500

    env updated code

---
+++ config/express.config.js
@@ -0,0 +1,32 @@
+/******************************************************
+ *************+ Dependencies *************** 
+ ******************************************************/
+
+const cors = require('cors'); 
+const bodyParser = require('body-parser'); 
+const fs = require('fs');   
+// const express = require('express'); //? --** Use if necessary **--    
+const corsOptions = {
+    exposedHeaders: ['token']
+}
+
+//  EXPOSTS
+module.exports = (app) => {
+
+    // Enabling Cors
+    app.use(cors(corsOptions));
+
+    // Pars application/JSON
+    app.use(bodyParser.json());
+
+    // Pars application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    // Setting Path to Route Folder
+    var routPath = './routes/';
+    fs.readdirSync(routPath).forEach((file) => {
+        var route = routPath + file;
+        require('../'+route)(app);
+    })
+
+}
